b5373a999de9f74362878a46b87233a4dfbabc35
AlejoVR09 <Alejandrovanegas09@outlook.com>
2024-01-20 14:59:57 -0500

    Navigation with Code and visual interaciton, Navigation with code

--- Navigation/ViewController.swift
+++ Navigation/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var PassToOther: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func PassTo(_ sender: Any) {
+        self.performSegue(withIdentifier: "SegueToListView", sender: nil)
+    }
+    @IBAction func PassToWithCode(_ sender: Any) {
+        let controller = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "ListView")
+        controller.modalPresentationStyle = .automatic
+        self.present(controller, animated: true)
+    }
+    
 }
 
